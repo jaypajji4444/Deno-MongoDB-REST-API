86da15f3684882fc2f492234d249aa942eb583ba
jaypajji4444 <43105921+jaypajji4444@users.noreply.github.com>
2020-05-21 15:24:57 +0530

    Add:DB file modified

--- DB.ts
+++ DB.ts
@@ -1,12 +1,27 @@
 import { MongoClient } from "https://deno.land/x/mongo@v0.7.0/mod.ts";
 
 
+
+console.log(Deno.env.get("DB_HOST"))
+const dbHost=Deno.env.get("DB_HOST") || "mongodb://localhost:27017";
+const dbName=Deno.env.get("DB_NAME") || "denoFirst";
 // connecting to mongCleint server
 const client=new MongoClient;
+let db;
+
+try{
 // Connection 
-client.connectWithUri("mongodb://localhost:27017");
+client.connectWithUri(dbHost);
 // Database
-const db=client.database("denoFirst")
+db=client.database(dbName)
+console.log("Connection Successfully to Database:",dbName)
+
+}
+catch(err){
+    console.log(err)
+}
+
+
     
 export default db;
 
