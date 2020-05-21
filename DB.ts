d4c5f81d13b7969d0da5e05638600ec46f316df1
jaypajji4444 <43105921+jaypajji4444@users.noreply.github.com>
2020-05-21 15:42:21 +0530

    Add:env loading

--- DB.ts
+++ DB.ts
@@ -1,25 +1,24 @@
 import { MongoClient } from "https://deno.land/x/mongo@v0.7.0/mod.ts";
+// Using .env file
+import {load} from "https://deno.land/x/denv/mod.ts";
+await load("./.env")
 
 
 
-console.log(Deno.env.get("DB_HOST"))
 const dbHost=Deno.env.get("DB_HOST") || "mongodb://localhost:27017";
 const dbName=Deno.env.get("DB_NAME") || "denoFirst";
 // connecting to mongCleint server
 const client=new MongoClient;
-let db;
 
-try{
+
+
 // Connection 
 client.connectWithUri(dbHost);
 // Database
-db=client.database(dbName)
+
+const db=client.database(dbName)
 console.log("Connection Successfully to Database:",dbName)
 
-}
-catch(err){
-    console.log(err)
-}
 
 
     
--- server.ts
+++ server.ts
@@ -1,4 +1,6 @@
 import {Application } from "https://deno.land/x/oak/mod.ts";
+
+
 import router from "./routes.ts";
 
 const app = new Application()
@@ -9,4 +11,5 @@ app.use(router.routes())
 app.use(router.allowedMethods())
 
 console.log("Server is up on port",port)
-await app.listen({port})
+app.listen({port})
+
